29d8b02b0f7a2882e3dd0893145ce9e4b323930f
Gerard Lardizabal <gerardlardizabal@gmail.com>
2019-12-02 12:11:16 +0800

    DynamicView

---
+++ DynamicFormsComponent/Components/DynamicFormFieldComponent/DynamicFormFieldComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Base.Core.App.Views.DynamicFormsComponent.Components.DynamicFormFieldComponent
+{
+    [ViewComponent(Name ="DynamicFormField")]
+    public class DynamicFormFieldComponent : ViewComponent
+    {
+        public DynamicFormFieldComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(DynamicForms.Objects.FormFields.FormField field)
+        {
+            return View("~/Views/DynamicFormsComponent/Components/DynamicFormFieldComponent/DynamicFormFieldComponent.cshtml",field);
+        }
+    }
+}
---
+++ DynamicFormsComponent/DynamicFormsViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamicForms;
+
+namespace Base.Core.App.Views.DynamicFormsComponent
+{
+    [ViewComponent(Name = "DynamicForm")]
+    public class DynamicFormsViewComponent : ViewComponent
+    {
+
+        DynamicForms.Objects.Forms forms;
+        public DynamicFormsViewComponent(DynamicForms.Interfaces.IFormService formService)
+        {
+            forms = new DynamicForms.Objects.Forms(formService);
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Guid formId)
+        {
+            DynamicForms.Objects.Forms.Form form = forms.GetForm(formId).FirstOrDefault();
+            return View("~/Views/DynamicFormsComponent/DynamicFormViewComponent.cshtml",form);
+        }
+    }
+}
